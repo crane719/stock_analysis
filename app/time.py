4c5c21394cb7746f78cbb34db21dc674b1025f28
crane719 <tmp.utils@gmail.com>
2020-04-20 02:31:44 +0900

    移動平均の追加

--- app/time.py
+++ app/time.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, jsonify, Response
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
 import finance
-import json
 import datetime as dt
+import numpy as np
 
 time = Blueprint('time', __name__)
 
@@ -26,3 +26,19 @@ def get_timeseries():
     times.index = times.index.astype(str)
     jsons = times[stock_type].to_json(orient="split")
     return jsonify(jsons)
+
+@time.route("/moving_average", methods=["POST"])
+def moving_average():
+    datas = request.json
+    point = int(datas["point"].split("pt")[0])
+    del datas["point"]
+
+    tmp_datas = {}
+    if type(datas["labels"])==str:
+        datas["labels"] = [datas["labels"]]
+        datas["datas"] = [datas["datas"]]
+    for key, data in zip(datas["labels"], datas["datas"]):
+        data = finance.moving_average(data, point)
+        key += "ma%d"%(point)
+        tmp_datas[key] = data
+    return jsonify(tmp_datas)
--- finance.py
+++ finance.py
@@ -1,6 +1,20 @@
 import quandl
+import numpy as np
 
 def get(api, brand):
     quandl.ApiConfig.api_key = api
     data = quandl.get(dataset=brand, returns="pandas")
     return data
+
+def moving_average(data, point):
+    data_len = len(data)
+    half_len = point//2
+    pad = [0 for _ in range(half_len)]
+    data = pad + data
+    data = data + pad
+
+    renew = []
+    for i in range(data_len):
+        renew.append(np.average(data[i:i+half_len*2+1]))
+    return renew
+
